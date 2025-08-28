2107e546ca47ea5a248cda5dc19153e7fabda1be
tem_5028 <gyeomspider10@naver.com>
2025-08-28 19:16:21 +0900

    v0.4 README.md

---
+++ week02/src/HelloWorld.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.next();
+        System.out.println("안녕 " + name);
+    }
+}
