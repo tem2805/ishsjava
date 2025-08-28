60d2f659a2e53ea9e2090fe0397c350f6ea8f1ae
tem2805 <gyeomspider10@naver.com>
2025-08-28 21:41:17 +0900

    v0.9 DataType & Casting

--- week03/src/DataType.java
+++ week03/src/DataType.java
@@ -1,5 +1,6 @@
 public class DataType {
     public static void main(String[] args) {
+        // 충돌 해결 하는 법
         //데이터 타입과 형변환 (Data type and Casting)
         int score = 87;
         float gpa = 4.13F;
