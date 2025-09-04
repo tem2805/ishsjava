2cffa6068e652b9c1bc8bb03142da232c217e876
tem_5028 <gyeomspider10@naver.com>
2025-09-04 21:10:39 +0900

    v1.5 for&if

--- week04/src/GugudanGame.java
+++ week04/src/GugudanGame.java
@@ -5,9 +5,12 @@ public class GugudanGame {
         Scanner scanner = new Scanner(System.in);
         System.out.print("단을 입력하세요");
         int dan = scanner.nextInt();
-
-        for(int i=1; i<10; i++){
-            System.out.println(dan + " x " + i + " = " +(dan*i));
+        if(dan < 2 || dan > 9) {
+            System.out.println("단은 2~9 사이의 값입니다.");
+        }else{
+            for(int i=1; i<10; i++) {
+                System.out.println(dan + " x " + i + " = " + (dan * i));
+            }
         }
     }
 }
