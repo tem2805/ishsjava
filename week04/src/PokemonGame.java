0cdaf8b30af81aa18dc1ed830c8953b9359873ed
tem_5028 <gyeomspider10@naver.com>
2025-09-04 20:38:57 +0900

    v1.3 if,else if,else

--- week04/src/PokemonGame.java
+++ week04/src/PokemonGame.java
@@ -1,3 +1,6 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 class Pokemon{
     //concrete class (can produce object)
     int hp;
@@ -7,9 +10,23 @@ public class PokemonGame {
     public static void main(String[] args) {
         Pokemon pikachu = new Pokemon();
         Pokemon squirtle = new Pokemon();
-        pikachu.hp = 100;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("체력을 입력하세요 : ");
+        pikachu.hp = scanner.nextInt();
         squirtle.hp = 110;
-        System.out.println(squirtle.hp);
-        System.out.println(pikachu.hp);
+//        System.out.println(squirtle.hp);
+//        System.out.println(pikachu.hp);
+
+        //if(pikachu.hp > 99){
+        if(pikachu.hp > squirtle.hp){
+            System.out.println("피카츄의 체력(" + pikachu.hp +")이 여유~");
+            System.out.println("꼬부기의 체력(" + squirtle.hp +")보다 높아요");
+        }else if(pikachu.hp < squirtle.hp){
+            System.out.println("피카츄의 체력(" + pikachu.hp +")이 부족!");
+            System.out.println("꼬부기의 체력(" + squirtle.hp +")보다 낮아요");
+        }else {
+            System.out.println("두 포켓몬스터의 체력이 동일합니다.");
+        }
     }
 }
