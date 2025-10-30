677e20bfb45bb3af1dc60a6582ffa53175f7d5d9
tem_5028 <gyeomspider10@naver.com>
2025-10-30 20:23:24 +0900

    exception handling v0.2

--- week08/src/ExceptionTest.java
+++ week08/src/ExceptionTest.java
@@ -4,10 +4,16 @@ public class ExceptionTest {
     public static void main(String[] args) {
         Scanner scanner = new Scanner (System.in);
         System.out.print("Input numerator : ");
+        //double numerator = scanner.nextDouble();
         int numerator = scanner.nextInt();
         System.out.print("Input denominator : ");
+        //double denominator = scanner.nextDouble();
         int denominator = scanner.nextInt();
 
-        System.out.println(numerator/denominator);
+        if(denominator == 0){
+            System.out.println("분모에 0이 올 수 없습니다!");
+        }else{
+            System.out.println(numerator/denominator);
+        }
     }
 }
