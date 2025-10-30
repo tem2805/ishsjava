aee74e61ea0599b0d1897fbda844f85e0a3c435a
tem_5028 <gyeomspider10@naver.com>
2025-10-30 20:41:46 +0900

    exception handling v0.4

--- week08/src/ExceptionTest.java
+++ week08/src/ExceptionTest.java
@@ -10,19 +10,15 @@ public class ExceptionTest {
 
         try{
             System.out.print("Input numerator : ");
-            //double numerator = scanner.nextDouble();
             numerator = scanner.nextInt();
             System.out.print("Input denominator : ");
-            //double denominator = scanner.nextDouble();
             denominator = scanner.nextInt();
+
+            System.out.println(numerator/denominator);
         }catch (InputMismatchException err){
             System.out.println("숫자로 입력하세요.");
-        }
-
-        if(denominator == 0){
-            System.out.println("분모에 0이 올 수 없습니다!"); // logical error
-        }else{
-            System.out.println(numerator/denominator);
+        }catch (ArithmeticException err){
+            System.out.println("분모에 0이 올 수 없습니다!");
         }
     }
 }
