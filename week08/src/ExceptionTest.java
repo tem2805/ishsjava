77e7ed150a88419000b0598ce61306a0e25d39b0
tem_5028 <gyeomspider10@naver.com>
2025-10-30 21:08:30 +0900

    exception handling v0.5

--- week08/src/ExceptionTest.java
+++ week08/src/ExceptionTest.java
@@ -17,8 +17,19 @@ public class ExceptionTest {
             System.out.println(numerator/denominator);
         }catch (InputMismatchException err){
             System.out.println("숫자로 입력하세요.");
+            System.out.println(err.getMessage());
         }catch (ArithmeticException err){
             System.out.println("분모에 0이 올 수 없습니다!");
+            System.out.println(err.getMessage());
+        }catch (ArrayIndexOutOfBoundsException err){
+            System.out.println("인덱스의 범위를 벗어났습니다.");
+            System.out.println(err.getMessage());
+        }catch (Exception err){  // 위에서 해결할 수 없는 예외처리
+            System.out.println("에러 발생");
+            System.out.println(err.getMessage());
+        }finally { // 예외 발생 여부에 상관 없이 항상 실행
+            System.out.println("프로그램을 종료합니다");
+            // 자원 해제 등의 필요할 때 사용
         }
     }
 }
